91ce5cf22bd15ac1fa8683c4241f9b4ecf85f1f5
Dmitry Pozdnyakov <pozdnyakovds@tatintec.ru>
2022-05-06 12:05:45 +0300

    Start

---
+++ src/AppButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="btn" :class="color" @click="$emit('action')">
+        {{ text }}
+    </button>
+</template>
+
+<script>
+    export default {
+        emits: ['action'],
+        props: {
+            text: String,
+            color: {
+                type: String,
+                default: '',
+                validator(value) {
+                    return ['', 'primery', 'danger'].includes(value)
+                }
+            }
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
